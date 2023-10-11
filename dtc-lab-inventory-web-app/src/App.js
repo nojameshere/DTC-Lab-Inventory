2c38c386b986aada36ef27a9afb803010ae82c24
James <39015355+nojameshere@users.noreply.github.com>
2023-10-11 08:11:26 -0600

    Setup firebase config. Need to discuss db options with sponsor

--- dtc-lab-inventory-web-app/src/App.js
+++ dtc-lab-inventory-web-app/src/App.js
@@ -2,8 +2,38 @@ import './App.css';
 import LogInPage from './pages/LogInPage.js';
 import Main from './pages/Main.js'
 import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  //firebase config needs to be put here. This will be done in a .env file and will
+  //need to be otained from George Ray
+  //Some of these will not be needed as I am reusing stuff from another project
+  //doing config like this is secure, if you ever fork and re-upload to github please
+  //make sure your .env file is on your gitignore list.
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DB_URL}`,
+  projectID: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messageSenderID: `${process.env.REACT_APP_APP_MESS_SEND_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
 function App() {
-  var loggedIn = false;
+  var loggedIn = () => {
+
+    //this needs to be updated once a more secure auth process is set up
+    if(localStorage.getItem('accessToken') !== null){
+      return true;
+    }
+    else{return false;}
+  }
 
 
   return (
--- dtc-lab-inventory-web-app/src/pages/LogInPage.js
+++ dtc-lab-inventory-web-app/src/pages/LogInPage.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './LogInPage.css';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+
 
 export default function LogInPage() {
-    const handleLogin = () => {
-        //login Logic here
+    const [errorMessage, setErrorMessage] = useState('');
+    const handleLogin = async (event) => {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const auth = getAuth();
+        try{
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user;
+            //The code below needs to be changed to be more secure when time allots.
+            //The place checking for the access token also needs to be updated for better security
+            localStorage.setItem('accessToken', user.accessToken);
+            localStorage.setItem('uID', user.uid);
+            window.location.reload();
+        }
+        catch(error){
+            setErrorMessage('Invalid Email or Password');
+        }
+
         console.log('button pressed')
     }
 
@@ -13,7 +32,7 @@ export default function LogInPage() {
                 <button className="logInCancel">Cancel</button>
             </div>
             <div className="logInContent">
-                <input className="logInInput" placeholder="username" id="username" autoComplete="none"></input>
+                <input className="logInInput" placeholder="email@domain.com" id="email" autoComplete="none"></input>
                 <input className="logInInput" placeholder="password" id="password" type="password"></input>
                 <button className="logInButton" onClick={handleLogin}>Log In</button>
             </div>
